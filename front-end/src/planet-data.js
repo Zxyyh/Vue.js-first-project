78e3f90827b9e044912034b40e03c8a9d8241203
Zxyyh <rinjaniadri@gmail.com>
2022-11-21 13:45:13 +0700

    push update

--- front-end/src/planet-data.js
+++ front-end/src/planet-data.js
@@ -20,7 +20,7 @@ export const planetChartData = {
     datasets: [
       {
         label: 'Number of Moons',
-        data: [0, 0, 1, 2, 79, 82, 27, 14],
+        data: [0, 0, 1, 2, 79, 82, 27],
         backgroundColor: 'rgba(54,73,93,.5)',
         borderColor: '#36495d',
         borderWidth: 3
